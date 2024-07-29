1b76bf5a146dc2a6bccaa138e43f78f6972db725
Wiranphat Moolmee <138298895+ranb27@users.noreply.github.com>
2024-07-29 19:42:25 +0700

    Remove unused imports and commented code

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,7 @@
-import DeployButton from "../components/DeployButton";
-import AuthButton from "../components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
-import Header from "@/components/Header";
 import hero_image from "../public/hero_image.jpg";
 import Link from "next/link";
 
-export default async function Index({
-  Component,
-  pageProps,
-}: {
-  Component: any;
-  pageProps: any;
-}) {
-  // const canInitSupabaseClient = () => {
-  //   // This function is just for the interactive tutorial.
-  //   // Feel free to remove it once you have Supabase connected.
-  //   try {
-  //     createClient();
-  //     return true;
-  //   } catch (e) {
-  //     return false;
-  //   }
-  // };
-
-  // const isSupabaseConnected = canInitSupabaseClient();
-
+export default function Index() {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <div
